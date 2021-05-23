f54f0b746b6586d3505d70c2d6e6eb3c2bc3d0b6
Codeclan Student <student@Codeclans-MacBook-Pro.local>
2021-05-23 19:36:46 +0100

    first

---
+++ rockpaperscissors_weekendhomework-main/controllers/controller.py
@@ -0,0 +1,26 @@
+from flask import render_template, request, redirect
+from flask.wrappers import Request
+from werkzeug.utils import redirect
+from app import app
+from modules import game
+from modules import player
+
+@app.route('/rps')
+def index():
+    return render_template('base.html')
+
+@app.route('/rps/play')
+def name_entry():
+    return render_template('new_game.html')
+
+@app.route('/rps/play/rules',methods=['GET'])
+def game_rules():
+    return render_template('how-to-play.html')
+
+# @app.route('/rps/play/<choice_1>/<choice_2>')
+# def play(choice_1,choice_2):
+#     return f"The winner is {game.play(choice_1,choice_2)}"
+
+@app.route('/rps/play/<choice_1>/<choice_2>')
+def play(choice_1,choice_2):
+    return f"The winner is {game.play(choice_1,choice_2)}"
